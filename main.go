49df561dceb06aaac6b6a281b2a359ab757b0882
Jarvis <jarvis@sauce.sh>
2026-03-01 06:19:30 -0800

    move main.go to root for clean go install path

---
+++ main.go
@@ -0,0 +1,24 @@
+// Package main is the entry point for the create-go-app scaffolder.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/builderjarvis/create-go-app/cli"
+
+	// Register all features via blank imports.
+	_ "github.com/builderjarvis/create-go-app/features/docker"
+	_ "github.com/builderjarvis/create-go-app/features/httpclient"
+	_ "github.com/builderjarvis/create-go-app/features/postgres"
+	_ "github.com/builderjarvis/create-go-app/features/retry"
+	_ "github.com/builderjarvis/create-go-app/features/state"
+	_ "github.com/builderjarvis/create-go-app/features/worker"
+)
+
+func main() {
+	if err := cli.Run(os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
